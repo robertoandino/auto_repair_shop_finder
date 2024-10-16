19b314b209b21aed7e33f475e1282e7045fee322
Roberto Andino <andinorobert518@yahoo.com>
2024-10-16 19:16:39 -0400

    Added some CSS

--- src/frontend/components/SearchFilters.js
+++ src/frontend/components/SearchFilters.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import './SearchFilters.css';
 
 const SearchFilters = ({ onSearch }) => {
 
@@ -12,25 +13,40 @@ const SearchFilters = ({ onSearch }) => {
     }
 
     return (
-        <form onSubmit={handleSubmit}>
-            <label>
-                Car Model:
-                <input 
-                    type="text"
-                    value={model}
-                    onChange={(e) => setModel(e.target.value)}
-                />
-            </label>
-            <label>
-                Service Type:
-                <input
-                    type="text"
-                    value={service}
-                    onChange={(e) => setService(e.target.value)}
-                />
-            </label>
-            <button type="submit">Search</button>
-        </form>
+        <div className="search-filters-card">
+            <h2 className="search-title">Find Your Repair Shop</h2>
+            <form className="search-filters" onSubmit={handleSubmit}>
+                <div className="filter-input">
+                    <label htmlFor="car-model">Car Model:</label>
+                    <div className="input-wrapper">
+                        <i className="fa fa-car" aria-hidden="true"></i>
+                        <input 
+                            type="text"
+                            id='car-model'
+                            value={model}
+                            onChange={(e) => setModel(e.target.value)}
+                            placeholder="Enter your car model"
+                        />
+                    </div>
+                </div>
+                <div className="filter-input">
+                    <label htmlFor="service-type">Service Type:</label>
+                    <div className="input-wrapper">
+                        <i className="fa fa-wrench" aria-hidden="true"></i>
+                        <input 
+                            type="text"
+                            id='service-type'
+                            value={service}
+                            onChange={(e) => setService(e.target.value)}
+                            placeholder="Enter service type"
+                        />
+                    </div>
+                </div>
+                <button type="submit" className="search-button">
+                    <i className="fa fa-search" aria-hidden="true"></i>Search
+                </button>
+            </form>
+        </div>
     );
 };
 
